08908d408e7729697cff2f29a4b5dd1839830722
Michael Krämer <info.m.kraemer@gmail.com>
2024-05-19 23:16:37 +0200

    create contact and edit contact slide effect and popUP

--- js/contacts.js
+++ js/contacts.js
@@ -1,3 +1,6 @@
+let contactColors = [ '#FF7A00', '#FF5EB3', '#6E52FF', '#9327FF', '#00BEE8', '#1FD7C1', '#FF745E', '#FFA35E', '#FC71FF', '#FFC701', '#0038FF', '#C3FF2B', '#FFE62B', '#FF4646', '#FFBB2B'];
+
+
 function displayGreyBackground() {
     document.getElementById('greyBackground').classList.remove('d-none');
     greyBackground.classList.remove('removeGreyBackgroundOpacity');
@@ -16,28 +19,27 @@ function removeGreyBackground(){
 
 
 function closeAddContactPopUp() {
-    slideOut();    
+    slideOut();
     removeGreyBackground();
-
 }
 
 
 function slideOut() {
-    let popUp = document.getElementById('popUp');
-    popUp.classList.add('slideOut');
-    popUp.classList.remove('slideIn');
+    let contactPopUp = document.getElementById('contactPopUp');
+    contactPopUp.classList.add('slideOut');
+    contactPopUp.classList.remove('slideIn');
 }
 
 
 function slideIn() {
-    let popUp = document.getElementById('popUp');
-    popUp.classList.add('slideIn');
-    popUp.classList.remove('slideOut');
+    let contactPopUp = document.getElementById('contactPopUp');
+    contactPopUp.classList.add('slideIn');
+    contactPopUp.classList.remove('slideOut');
 }
 
 
 function openAddContactPopUp() {
-    document.getElementById('popUp').innerHTML = addContactFormHTML(); 
+    document.getElementById('contactPopUp').innerHTML = addContactFormHTML(); 
     slideIn();
     displayGreyBackground();
 }
@@ -79,3 +81,47 @@ function addContactFormHTML() {
         </div>
     `;
 }
+
+
+function openEditPopUp() {
+    document.getElementById('contactPopUp').innerHTML = editContactFormHTML(); 
+    slideIn();
+    displayGreyBackground();
+}
+
+function editContactFormHTML() {
+    return /*html*/`
+        <div class="addContactPopUpTitleContainer">
+            <img class="addContactJoinLogo" src="./assets/img/joinLogoSmallWhite.svg">
+            <h2 class="popUpTitle">Edit contact</h2>
+            <div class="blueHorizontalLine">
+            </div>
+        </div>
+
+        <div class="addContactFormContainer">
+            <img onclick="closeAddContactPopUp()" class="popUpRightCornerCloseButton" src="./assets/img/cancelX.svg">
+            <div class="contactFormAndImgContainer">
+                <img class="contactPopUpProfileImg" src="./assets/img/ProfileImg.svg">
+                <form class="formContainer" action="onsubmit">
+                    <div class="contactsInputContainer">
+                        <input type="text" required class="nameEmailTel" placeholder="Name">
+                        <img src="./assets/img/person.svg" class="contactsInputIcon">
+                    </div>
+                    <div class="contactsInputContainer">
+                        <input type="email" required class="nameEmailTel" placeholder="Email">
+                        <img src="./assets/img/mail.svg" class="contactsInputIcon">
+                    </div>
+                    <div class="contactsInputContainer">
+                        <input type="tel" required pattern="[0-9]{10}" class="nameEmailTel" placeholder="Phone">
+                        <img src="./assets/img/call.svg" class="contactsInputIcon">
+                    </div>
+                    <div class="cancelAndCreateContainer">
+                        <button onclick="closeAddContactPopUp()" class="contactCancelButton">Cancel <img class="addContactCancelX" src="./assets/img/cancelX.svg"></button>
+                        <button class="contactCreateButton">Edit Contact <img src="./assets/img/miniCheckIcon.svg"></button>
+                    </div>                  
+                </form>
+            </div>
+        </div>
+    `;
+}
+
