f73ff2bfdfe68b69eda5a8e7d91ddd728559277b
AndrejYaotsin <andrej.butalov@gmx.de>
2024-05-27 20:41:36 +0200

    board.js/dbCRUD/

--- js/add_task.js
+++ js/add_task.js
@@ -1,24 +1,26 @@
 
 
-function initAddTask() {
-    let add_task_form = document.getElementById('add_task_form');
+ async function initAddTask() {
+    
+    let add_task_form = document.getElementById('add_task_form'); 
+          
 
-    add_task_form.innerHTML = `
+    add_task_form.innerHTML = /*html*/`
     <h1>Add Task</h1>
-    <form action="">
         <div class="add_task_form">
             <div class="add_task_width50">
                 <div class="add_task_title add_task_form_row">
                     <label for="">Title<b>*</b></label>
-                    <input class="add_task_input" type="text" placeholder="Enter a title" required>
+                    <!-- required -->
+                    <input id="task_title" class="add_task_input" type="text" placeholder="Enter a title" >
                 </div>
                 <div class="add_task_descripion add_task_form_row">
                     <label for="">Description</label>
-                    <textarea class="add_task_textarea" name="" id="" placeholder="Enter a Description"></textarea>
+                    <textarea id="task_description" class="add_task_textarea" name="" id="" placeholder="Enter a Description"></textarea>
                 </div>
                 <div class="add_task_assignet add_task_form_row">
                     <label for="">Assignet to</label>
-                    <select class="add_task_input" name="select">
+                    <select id="task_assignet" class="add_task_input" name="select">
                         <option value="" hidden>Select contact to assign</option>
                         <option value="">Name 1</option>
                         <option value="">Name 2</option>
@@ -32,7 +34,8 @@ function initAddTask() {
             <div class="add_task_width50">
                 <div class="add_task_date add_task_form_row">
                     <label for="">Due date<b>*</b></label>
-                    <input class="add_task_input" type="date" required>
+                    <!--  required-->
+                    <input id="task_date" class="add_task_input" type="date" >
                 </div>
                 <div class="add_task_prio add_task_form_row">
                     <p>Prio</p>
@@ -56,7 +59,8 @@ function initAddTask() {
                 </div>
                 <div class="add_task_category add_task_form_row">
                     <label for="">Categoriy<b>*</b></label>
-                    <select class="add_task_input" name="" id="" required>
+                    <!--required  -->
+                    <select id="task_category" class="add_task_input" name="" id="" >
                         <option value="" hidden>Select task categoriy</option>
                         <option value="">Technical Task</option>
                         <option value="">User Story</option>
@@ -64,7 +68,7 @@ function initAddTask() {
                 </div>
                 <div class="add_task_subtask add_task_form_row">
                     <label for="">Subtasks</label>
-                    <select class="add_task_input" name="" id=""></select>
+                    <select id="task_subtasks" class="add_task_input" name="" id=""></select>
                 </div>
             </div>
 
@@ -73,11 +77,36 @@ function initAddTask() {
             <p><b>*</b>This field is required</p>
             <div class="add_task_button_group_footer">
                 <button class="add_task_button_clear add_task_hover_button">Clear X</button>
-                <button class="add_task_button_create add_task_hover_button">Create Task
+                <button class="add_task_button_create add_task_hover_button" onclick="addTaskToTasks()">Create Task
                     <img src="./assets/img/vector_check.svg">
                 </button>
             </div>
         </div>
-    </form>  
     `;
+}
+
+async function addTaskToTasks() {
+
+    let task_title = document.getElementById('task_title').value;
+    let task_description = document.getElementById('task_description').value;
+    let task_assignet = document.getElementById('task_assignet').value;
+    let task_date = document.getElementById('task_date').value;
+    let task_category = 'done';
+    let task_status = document.getElementById('task_category').value;
+    let task_subtasks = document.getElementById('task_subtasks').value;
+
+    let task = {
+        'category': task_category,
+        'date': task_date,
+        'description': task_description,
+        'id': 'id',
+        'name': task_assignet,
+        'priority': './assets/img/vector_check.svg',
+        'status': task_status,
+        'subtask': task_subtasks,
+        'title': task_title    
+    };
+
+    postTasksToDb("/tasks", task);
+    initAddTask();
 }
--- js/board.js
+++ js/board.js
@@ -1,99 +1,127 @@
-let todos = [{
-    'id': 0,
-    'title': 'Kochwelt Page & Recipe',
-    'description': 'Build start page with recipe recommendation...',
-    'priority': './assets/img/vector_strich.svg',
-    'status': 'User Story',
-    'category': 'to_do',
-}, {
-    'id': 1,
-    'title': 'Kochen',
-    'description': 'Build start page with recipe recommendation...',
-    'priority': './assets/img/vector_strich.svg',
-    'status': 'User Story',
-    'category': 'to_do'
-}, {
-    'id': 2,
-    'title': 'Einkaufen',
-    'description': 'Build start page with recipe recommendation...',
-    'priority': './assets/img/vector_strich.svg',
-    'status': 'Technical Task',
-    'category': 'in_progress'
-},
-{
-    'id': 3,
-    'title': 'Await',
-    'description': 'Build start page with recipe recommendation...',
-    'priority': './assets/img/vector_strich.svg',
-    'status': 'Technical Task',
-    'category': 'await'
-},
-{
-    'id': 4,
-    'title': 'Done',
-    'description': 'Build start page with recipe recommendation...',
-    'priority': './assets/img/vector_strich.svg',
-    'status': 'User Story',
-    'category': 'done'
-}];
-
+let todos = [];
 let currentElement;
 
+const BASE_URL = "https://tasks-6f30e-default-rtdb.europe-west1.firebasedatabase.app/";
 
-function init() {
+async function loadTasksFromDb(path="") {
+    let response = await fetch(BASE_URL + path + ".json");
+    return responseAsJson = await response.json();
+}
+
+
+async function postTasksToDb(path="", data={}) {
+    let response = await fetch(BASE_URL + path + ".json", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data)
+    });
+    return responseAsJson = await response.json();
+}
 
 
 
+async function deleteTaskFromDb(arr, i) {
+   console.log(arr);
+   console.log(i);
+   // let response = await fetch(BASE_URL + path + ".json", {
+   //     method: "DELETE",        
+   // });
+   // return responseAsJson = await response.json(); 
+}
+
+
+
+async function init() {
+    
+    // let taskID = document.getElementById('board_to_do');    
     let task = document.getElementById('board_to_do');
     let progress = document.getElementById('board_in_progress');
     let awaitFeedback = document.getElementById('board_await_feedback');
     let doneId = document.getElementById('board_done');
 
+
+    let data = await loadTasksFromDb("/tasks");    
+    
+    for (let index in data) {
+        let task = data[index];
+        todos.push(task);          
+    }
+
     let toDo = todos.filter(t => t['category'] == 'to_do');
     let inProgress = todos.filter(t => t['category'] == 'in_progress');
-    let await = todos.filter(t => t['category'] == 'await');
+    let feedback = todos.filter(t => t['category'] == 'await');
     let done = todos.filter(t => t['category'] == 'done');
 
-    generateToDo(toDo, task);
-    generateToDo(inProgress, progress);
-    generateToDo(await, awaitFeedback);
-    generateToDo(done, doneId);  
+    generateToDo(toDo, task, data);
+    generateToDo(inProgress, progress, data);
+    generateToDo(feedback, awaitFeedback, data);
+    generateToDo(done, doneId, data);  
+
+    
+
+    // let todos = await loadTasksFromDb("/tasks"); 
+    // console.log(todos);
+
+
+    // let task = document.getElementById('board_to_do');
+    // let progress = document.getElementById('board_in_progress');
+    // let awaitFeedback = document.getElementById('board_await_feedback');
+    // let doneId = document.getElementById('board_done');
+
+
+    // let toDo = todos.filter(t => t['category'] == 'to_do');
+    // let inProgress = todos.filter(t => t['category'] == 'in_progress');
+    // let feedback = todos.filter(t => t['category'] == 'await');
+    // let done = todos.filter(t => t['category'] == 'done');
+
+
+    // generateToDo(toDo, task);
+    // generateToDo(inProgress, progress);
+    // generateToDo(feedback, awaitFeedback);
+    // generateToDo(done, doneId);  
+
     
 }
 
 
-function generateToDo(arr, categorie_id) {
+
+
+function generateToDo(arr, categorie_id, data) {
 
     categorie_id.innerHTML = '';
 
+    let keys = Object.keys(data);
+
     for (let i = 0; i < arr.length; i++) {
+        let idkeys = keys[i];
         const element = arr[i];
 
         categorie_id.innerHTML += /*html*/`
-        <div class="task" draggable="true" ondragstart="startDragging(${element['id']})">
-            <div class="board_task_category">
-                ${element['status']}                
-            </div>
-            <div class="board_task_title">
-                ${element['title']}
-            </div>
-            <div class="board_task_toDo">
-                ${element['description']}
-            </div>
-            <div class="board_task_footer">
-                <div class="board_task_footer_names"></div>
-                <div class="board_task_footer_status">
-                    <img src="${element['priority']}">
-                </div>
-            </div>
-        </div>`
+        <div class="task" draggable="true" ondragstart="startDragging(${idkeys})">
+        <div class="board_task_category">${element['category']}</div>
+        <div class="board_task_title">${element['title']}</div>
 
-    }
+        <!-- <div>${idkeys}</div> -->
+
+        <div class="board_task_toDo">${element['description']}</div>
+        <div class="board_task_footer">
+
+        </div>
+        <div class="board_task_footer_status">
+                <img src="${element['priority']}">
+            </div>
+        </div>
+    `; 
+        
+    } 
 }
 
 
 
 function startDragging(id) {
+    console.log('hallo');
     currentElement = id;
 }
 
@@ -106,8 +134,9 @@ function allowDrop(ev) {
 
 
 function moveTo(category) {
-    todos[currentElement]['category'] = category;
-    init();
+    // console.log(todos);
+    // todos[currentElement]['category'] = category;
+    // init();
 }
 
 
