045fb5178581767c390314e7a4e680cf103d077e
Sascha Böse <saschaboese1996@gmail.com>
2024-06-16 12:07:37 +0200

    generate NoTask if array empty

--- js/board.js
+++ js/board.js
@@ -64,7 +64,7 @@ async function initBoardTasks() {
     let feedback = todos.filter(t => t['category'] == 'await');
     let done = todos.filter(t => t['category'] == 'done');
 
-    generateNoTask(toDo)
+    generateNoTask(toDo);
     generateToDo(toDo, task);
     generateToDo(inProgress, progress);
     generateToDo(feedback, awaitFeedback);
@@ -74,32 +74,30 @@ async function initBoardTasks() {
 
 async function generateToDo(arr, categorie_id) {
     categorie_id.innerHTML = '';
-    for (let i = 0; i < arr.length; i++) {
-        const element = arr[i];
-        categorie_id.innerHTML += renderHtmlToDo(element);
-        let idSUb = document.getElementById(`idSUb${element.id}`);
+    if (arr.length) {
+        for (let i = 0; i < arr.length; i++) {
+            const element = arr[i];
+            categorie_id.innerHTML += renderHtmlToDo(element);
+            let idSUb = document.getElementById(`idSUb${element.id}`);
 
-        if (element.subtasks) {
-            idSUb.innerHTML = '';
-            idSUb.innerHTML = renderHtmlProgressBarEmpty(element)
-            if (element.selectedTask) {
+            if (element.subtasks) {
                 idSUb.innerHTML = '';
-                idSUb.innerHTML += renderHtmlProgressBar(element);
+                idSUb.innerHTML = renderHtmlProgressBarEmpty(element)
+                if (element.selectedTask) {
+                    idSUb.innerHTML = '';
+                    idSUb.innerHTML += renderHtmlProgressBar(element);
+                }
             }
-        }        
-        getInitialsArray(element);
-        getCategorieBackGroundColor(element);
+            getInitialsArray(element);
+            getCategorieBackGroundColor(element);
+        }
+    } else {
+        generateNoTask(categorie_id);
     }
 }
 
-
-function generateNoTask(toDo) {
-    let no_task_todo = document.getElementById('no_task_todo');
-    if (toDo.length) {
-        no_task_todo.style.visibility = 'hidden';
-    } else {
-        no_task_todo.style.visibility = 'initial';
-    }
+function generateNoTask(categorie_id) {
+    categorie_id.innerHTML += `<div class="no_task">No tasks</div>`
 }
 
 
@@ -476,11 +474,11 @@ async function upgradeTodos(id) {
     await saveTasksToServer();
     initAddTask();
     initBoardTasks();
-    closeShowTask();    
+    closeShowTask();
 }
 
 
-function getPriorityUpdateTodos(userPriotity) {    
+function getPriorityUpdateTodos(userPriotity) {
     switch (userPriotity) {
         case 'Urgent':
             priorityImgEdit = './assets/img/vector_red.svg';
